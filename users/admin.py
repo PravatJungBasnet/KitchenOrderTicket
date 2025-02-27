c6afa16e1b4af0cb8010d370a14ff67223c28017
Pravat Jung Basnet <pravatjungbasnet@gmail.com>
2025-02-27 21:51:49 +0545

    Initialize Model and Pre-commit

---
+++ users/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+# Register your models here.
+@admin.register(User)
+class UserModelAdmin(BaseUserAdmin):
+    model = User
+
+    list_display = ["email", "first_name", "is_admin", "is_active"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["first_name", "last_name"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "name", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+# Now register the new UserAdmin...
+
+
+# Register your models here.
